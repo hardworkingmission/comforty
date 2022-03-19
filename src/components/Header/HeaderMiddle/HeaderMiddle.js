7e121dd227d45690beb9b97a140c92e7239aa235
hardworkingmission <shakil20131101041@gmail.com>
2022-03-19 15:25:52 +0600

    header-complete-1.1

--- src/components/Header/HeaderMiddle/HeaderMiddle.js
+++ src/components/Header/HeaderMiddle/HeaderMiddle.js
@@ -6,8 +6,8 @@ const HeaderMiddle = () => {
         <div className='header-middle'>
             <div className="container">
                 <div className="row">
-                    <div className="col-lg-4 col-md-4 col-sm-12 col-12 py-1">
-                        <h4><i className="fas fa-couch"></i>Comforty</h4>
+                    <div className="col-lg-4 col-md-4 col-sm-12 col-12 py-1 text-lg-start text-md-start text-sm-center text-center">
+                        <h4 className='mb-0'><i className="fas fa-couch"></i>Comforty</h4>
                     </div>
                     <div className="col-lg-4 col-md-4 col-sm-12 col-12">
                         <div className="d-flex search-box justify-content-center py-2">
@@ -16,7 +16,7 @@ const HeaderMiddle = () => {
                         </div>
                     </div>
                     <div className="col-lg-4 col-md-4 col-sm-12 col-12">
-                        <div className='d-flex justify-content-end gap-3 py-2'>
+                        <div className='d-flex justify-content-lg-end justify-content-md-end justify-content-sm-center justify-content-center gap-3 py-2'>
                             <div className="cart bg-white py-1 px-2 rounded"><i className="fas fa-shopping-cart me-1"></i>Cart <div className="d-inline ms-1">1</div>
                             </div>
                             <div className="whitelist bg-white py-1 px-2 rounded"><i class="far fa-heart"></i></div>
